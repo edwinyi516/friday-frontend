c66afe6b5e7d95d05b3798bb5da54467dac5004b
apriljsu <apriljsu@gmail.com>
2022-09-27 16:05:13 -0400

    Members component added and tested

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from "./pages/Home"
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Member from './components/Members/Members.js'
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import Home from "./pages/Home"
+// import Dashboard from './pages/Dashboard';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </Router>
+    <h1>Friday.com Test</h1>
+    <Member />
     </>
+    // <>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/dashboard" element={<Dashboard />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/signup" element={<Signup />} />
+    //     </Routes>
+    //   </Router>
+    // </>
   );
 }
 
