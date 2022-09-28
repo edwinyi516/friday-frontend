2715be8f01db76bdbdb1695d35f4c1b39ef1cd7f
apriljsu <apriljsu@gmail.com>
2022-09-27 20:34:52 -0400

    reversed app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import './App.css';
-import Member from './components/Members/Members.js'
-import ProjectForm from './components/ProjectForm/ProjectForm.js'
-import Project from './components/Projects/Projects.js'
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import Home from "./pages/Home"
-// import Dashboard from './pages/Dashboard';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
-//add base URL
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home"
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 function App() {
   return (
-    <>
-    <h1>Friday.com Test</h1>
-    <Member />
-    <Project />
+      <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </>
-    // <>
-    //   <Router>
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/dashboard" element={<Dashboard />} />
-    //       <Route path="/login" element={<Login />} />
-    //       <Route path="/signup" element={<Signup />} />
-    //     </Routes>
-    //   </Router>
-    // </>
   );
 }
 
