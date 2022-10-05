43426cca0d9207bb3870b6135a78b218797522a8
Edwin <edwin.yi.128@gmail.com>
2022-10-05 18:34:27 -0500

    Login page baseURL changed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ import axios from "axios"
 // } else {
 //   baseURL = "https://friday-project-mgmt-backend.herokuapp.com";
 // }
-let baseURL = process.env.REACT_APP_BACKEND_URL
+let baseURL = "https://friday-project-mgmt-frontend.herokuapp.com"
 
 export default function Login() {
   const [loginEmail, setLoginEmail] = useState("")
